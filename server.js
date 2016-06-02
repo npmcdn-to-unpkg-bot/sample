dab235f9804ded1d5eadccae0fb0f98661fbfa88
Gilad Resisi <giladresisi@gmail.com>
2016-06-02 23:51:13 +0300

    u

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require('express');
-var routes = require('./routes');
+var qs = require('querystring');
 var AWS = require('aws-sdk');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
 var GetMethods = require('./server/GetMethods.js');
@@ -21,10 +19,30 @@ var db = new AWS.DynamoDB({region: "us-west-2"});
 
 //POST signup form.
 app.post('/signup', function(req, res) {
-  var nameField = req.body.name,
-      emailField = req.body.email,
-      previewBool = req.body.previewAccess;
-  signup(nameField, emailField, previewBool);
+  var body = '';
+
+  req.on('data', function (data) {
+      body += data;
+
+      // Too much POST data, kill the connection!
+      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+      if (body.length > 1e6)
+          req.connection.destroy();
+  });
+
+  req.on('end', function () {
+      var post = qs.parse(body);
+      var nameField = post.name,
+      emailField = post.email,
+      previewBool = post.previewAccess;
+      signup(nameField, emailField, previewBool);
+  });
+
+
+  // var nameField = req.body.name,
+  //     emailField = req.body.email,
+  //     previewBool = req.body.previewAccess;
+  // signup(nameField, emailField, previewBool);
   res.status(200).end();
 });
 
